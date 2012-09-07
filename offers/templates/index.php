038c26a96a11f5f56b710d1003a92b9832346a80
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2012-09-07 16:27:29 +0200

    Changed the tabs on the offer templates page to be the same as the tabs on the issue details page.

--- offers/templates/index.php
+++ offers/templates/index.php
@@ -10,94 +10,49 @@ $templates = fetch_offer_templates();
 
 <? begin_slot('head') ?>
 <style>
-  .tabs .block-header {
-    padding: 0;
-  }
-  .tabs .block-header li {
-    margin: 0;
-    display: inline-block;
-    width: auto;
-  }
-  .tabs .block-header h1 {
-    margin: 0;
-  }
-  .tabs .block-header a {
-    display: block;
-    text-decoration: none;
-	  border-right: 1px solid #BBB;
-    padding: .3em .5em;
-  }
-  .tabs .block-header a:hover {
-    background: #F60;
-    color: #FFF;
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#FF9944', endColorstr='#FF5500');
-    background: -o-gradient(linear, left top, left bottom, from(#F94), to(#F50));
-    background: -webkit-gradient(linear, left top, left bottom, from(#F94), to(#F50));
-    background: -moz-linear-gradient(top,  #F94,  #F50);
-    text-shadow: #000 0 1px 0;
-  }
-  .tabs .block-name.tab-active a {
-    color: #FFF;
-    background: #246;
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#6688AA', endColorstr='#224466');
-    background: -o-gradient(linear, left top, left bottom, from(#68A), to(#246));
-    background: -webkit-gradient(linear, left top, left bottom, from(#68A), to(#246));
-    background: -moz-linear-gradient(top,  #68A,  #246);
-    text-shadow: #000 0 1px 0;
-  }
-  .tabs .block-header li:first-child a {
-    border-radius: .3em 0 0 0;
-  }
-  .tabs .block-body {
-    display: none;
-  }
-  .tabs .block-body.tab-active {
-    display: block;
-    border-radius: 0 0 .5em .5em;
-  }
+.ui-tabs .ui-tabs-panel {
+  padding: 1em;
+}
 </style>
 <? append_slot() ?>
 
 <? begin_slot('js') ?>
 <script>
-  $(function() {
-    $('.deleteTemplate').hide();
+$(function()
+{
+  $('.deleteTemplate').hide();
 
-    $('.tabs li a').click(function() {
-      $('.block-body.tab-active, .block-name.tab-active').removeClass('tab-active');
-      $(this).parent().addClass('tab-active');
-      $(this.href.substring(this.href.indexOf('#'))).addClass('tab-active').find('textarea, input').first().focus();
-      
-      return false;
-    });
+  $('#offerTemplatesTabs').tabs();
 
-    $('.templates').change(function() {
-      var $fields = $(this).closest('.form-fields'),
-          $option = $(this[this.selectedIndex]),
-          tpl     = JSON.parse($option.attr('data-template')),
-          id      = this.value;
+  $('.templates').change(function()
+  {
+    var $fields = $(this).closest('.form-fields');
+    var $option = $(this[this.selectedIndex]);
+    var tpl = JSON.parse($option.attr('data-template'));
+    var id  = this.value;
 
-      tpl.name = $option.text();
+    tpl.name = $option.text();
 
-      for (var field in tpl) {
-        $('[name="template[' + field + ']"]', $fields).val(tpl[field]);
-      }
+    for (var field in tpl)
+    {
+      $('[name="template[' + field + ']"]', $fields).val(tpl[field]);
+    }
 
-      $fields.find('.deleteTemplate')[id == 0 ? 'hide' : 'show']();
-    });
+    $fields.find('.deleteTemplate')[id == 0 ? 'hide' : 'show']();
   });
+});
 </script>
 <? append_slot() ?>
 
 <? decorate("Szablony ofert") ?>
 
-<div class="block tabs">
-  <ul class="block-header aside">
-    <li><h1 class="block-name tab-active"><a href="#clients">Klienci</a></h1>
-    <li><h1 class="block-name"><a href="#intros">Uzgodnienia wstępne</a></h1>
-    <li><h1 class="block-name"><a href="#outros">Uzgodnienia końcowe</a></h1>
+<div id="offerTemplatesTabs">
+  <ul>
+    <li><a href="#clients">Klienci</a>
+    <li><a href="#intros">Uzgodnienia wstępne</a>
+    <li><a href="#outros">Uzgodnienia końcowe</a>
   </ul>
-  <div id="clients" class="block-body tab-active">
+  <div id="clients" class="block-body">
     <form method="post" action="save.php?type=client">
       <ol class="form-fields">
         <li><? render_offer_templates($templates, 'client') ?>
