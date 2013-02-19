7bdaa7e03b9aa501453d60246c65806ec4e5b1f8
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2013-02-19 20:51:58 +0100

    Resize big images to 1920x1080 px on upload.

--- catalog/products/images/upload.php
+++ catalog/products/images/upload.php
@@ -1,6 +1,7 @@
 <?php
 
 include __DIR__ . '/../../_common.php';
+include_once __DIR__ . '/../../../_lib_/wideimage/lib/WideImage.php';
 
 no_access_if_not_allowed('catalog/manage');
 
@@ -19,6 +20,8 @@ exec_insert('catalog_product_images', $bindings = array(
   'file' => substr(strrchr($_REQUEST['file'], DIRECTORY_SEPARATOR), 1),
   'description' => $dotPos === false ? $_REQUEST['name'] : substr($_REQUEST['name'], 0, $dotPos)
 ));
+
+WideImage::loadFromFile($file)->resize(1920, 1080, 'inside', 'down')->saveToFile($file);
 
 $bindings['id'] = (int)get_conn()->lastInsertId();
 
