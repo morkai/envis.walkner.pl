78a827f63020d651420f70d92bc8dd9251e71c57
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2013-02-19 12:07:05 +0100

    Fixed thumbnail generator not working if the original file's extension is not lowercase.

--- catalog/products/images/thumb.php
+++ catalog/products/images/thumb.php
@@ -4,17 +4,17 @@ include_once __DIR__ . '/../../_common.php';
 
 bad_request_if(empty($_GET['file']));
 
-preg_match('/^[a-z0-9]{32}\.(jpe?g|gif|png)$/', $_GET['file'], $matches);
+preg_match('/^[a-z0-9]{32}\.(jpe?g|gif|png)$/i', $_GET['file'], $matches);
 
 bad_request_if(empty($matches));
 
-$ext = $matches[1];
+$ext = strtolower($matches[1]);
 $type = 'image/' . ($ext === 'jpg' ? 'jpeg' : $ext);
 
 header("Content-Type: {$type}");
 
 $originalFile = ENVIS_UPLOADS_PATH . '/products/' . $_GET['file'];
-$thumbFile = ENVIS_UPLOADS_PATH . '/products/' . str_replace(".{$ext}", ".thumb.{$ext}", $_GET['file']);
+$thumbFile = ENVIS_UPLOADS_PATH . '/products/' . str_replace(".{$ext}", ".thumb.{$ext}", strtolower($_GET['file']));
 
 if (!file_exists($thumbFile))
 {
